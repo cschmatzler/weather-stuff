4108bf0e71639003daebe5eddb989b3e5f54170c
franca.winter <franca.winter@memsource.com>
2024-02-08 17:09:10 +0100

    Add daycard

--- app.vue
+++ app.vue
@@ -3,7 +3,10 @@
     <nav class="flex h-12 bg-black">
     </nav>
     <main class="flex-grow grid grid-cols-4 bg-blue-500">
-      <section class="bg-red-500 col-span-3">1</section>
+      <section class="bg-red-500 col-span-3 flex gap-4">
+        <DayCard/>
+        <DayCard/>
+      </section>
       <section>2</section>
       <section class="col-span-3">3</section>
       <section>4</section>
---
+++ components/DayCard/DayCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="bg-blue-300 rounded-xl overflow-hidden">
+    <header class="flex bg-blue-400 justify-between p-3">
+      <p>Monday</p>
+      <p>Time</p>
+    </header>
+    <div class="p-3">
+      <span>16°</span>
+      <span>SUN ICON</span>
+    </div>
+    <footer class="p-3">
+      More info..
+    </footer>
+  </section>
+</template>
