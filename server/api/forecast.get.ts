ad0129e19990831741b2161588e8b652aafee9f0
Bopp, Florian <florian.bopp90@gmail.com>
2024-03-08 09:23:13 +0100

    Refactor forecast.get.ts to use query validation and handle errors

--- server/api/forecast.get.ts
+++ server/api/forecast.get.ts
@@ -1,12 +1,32 @@
-export default defineEventHandler((event) => {
-  const query = getQuery(event);
-  console.log(query);
-  const lat = query.lat;
-  const lon = query.lon;
-  const apiKey = ""
-  const part = "minutely,hourly,alerts"
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}&units=metric`;
+import {OneCallApiResponseSchema, OneCallApiResponse} from '@/utils/openWeatherMap'
+import { z } from 'zod'
 
-  const response = $fetch(url);
-  return response;
+const querySchema = z.object({
+  lat: z.number(),
+  lon: z.number()
+})
+
+export default defineEventHandler(async (event) => {
+  const {owmApiSecret} = useRuntimeConfig(event)
+  // const query = getQuery(event);
+  // const lat = query.lat;
+  // const lon = query.lon;
+  const validatedQuery = await getValidatedQuery(event, body => querySchema.safeParse(body));
+  if (validatedQuery.success === false) {
+    console.error(validatedQuery.error);
+    return validatedQuery.error;
+  }
+  const { lat, lon } = validatedQuery.data;
+
+  const excludeParts = "minutely,hourly,alerts"
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${excludeParts}&appid=${owmApiSecret}&units=metric`;
+
+  try {
+    const response = $fetch(url);
+    const validatedResponse: OneCallApiResponse = OneCallApiResponseSchema.parse(response); 
+    return validatedResponse;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 })
